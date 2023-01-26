b54a4d0731b424c6635375a99a2b46240790a25e
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-01-26 22:31:14 +0600

    Add files via upload
    Template Literals & concat ...Arithmetic Basic Operators & Math

---
+++ main.js
@@ -0,0 +1,201 @@
+/*
+Template Literals & concat
+let x = 10;
+let y = 10;
+let result = x + y ;
+
+let html = `<h1>
+
+This Result is:${result}Number
+
+
+</h1>`
+document.body.innerHTML = html;
+*/
+
+/*
+Alert, Confirm, Prompt
+No:1,
+
+const checkAge = prompt("What is your age?",18)
+
+alert (`you are ${checkAge} years old`);
+*/
+
+/*
+No:2,
+
+const askQuestion = confirm("Are you sure?");
+
+const userAnswer = (askQuestion==true)?
+
+window.location = "https://google.com":alert(`Thangs for Staying`);
+*/
+
+/*
+No:3
+const askQuestion = confirm("Are you sure?");
+
+const userAnswer = (askQuestion==true)?
+
+window.location = "https://google.com":alert(`Thangs for Staying`);
+
+*/
+
+// Type Conversion
+
+// No:1;
+
+/*
+var num = 30;
+
+console.log(typeof(num));
+
+num = toString(num);
+
+console.log(typeof(num));
+
+No:2;
+
+var str = "30.50";
+
+console.log(typeof(str));
+
+str = parseFloat(str);
+
+console.log(typeof(str));
+
+console.log(typeof(parseFloat(30.50)));
+
+No:3;
+let barthYear = 1992;
+
+let makeString = String(barthYear);
+
+console.log(typeof(makeString));
+
+*/
+
+// Arithmetic Basic Operators & Math
+
+var x = 50;
+var y = 50;
+
+var resuld = x+y;
+
+console.log(resuld);
+
+// Exponentiation operator(power)
+
+// No:1
+var x = 5;
+var y = 4;
+
+var resuld = x**y;
+
+console.log (resuld);
+
+// No:2
+
+var x = 5;
+var y = 4;
+
+var resuld = x**y;
+
+console.log (Math.pow(x,y));
+
+
+// Increman ++(1Barba),Digreman -- (1comba);
+
+// No:1
+var x = 9;
+
+var y = 2;
+x++
+console.log(x);
+
+// No:2
+var x = 9;
+
+var y = 2;
+x--
+console.log(x);
+
+for(let index = 0; index < 100; index++){
+
+              console.log("The Number is:",index);
+}
+
+// string conversions Uniry
+
+// no:1
+
+let name = "Md" + " Khan";
+
+console.log(name);
+
+// no:2
+
+let apples = "50"
+
+let mango = "10"
+
+console.log(+apples + +mango);
+
+// no:3
+
+// Bitwise Operators
+// AND & &&;
+
+// RR | ||;
+
+// NOT~ !=;
+
+// no:3,A
+
+var x=20;
+
+var y=3;
+
+console.log(x % y);
+
+// no:3,B
+
+var x=20;
+
+var y=3;
+
+var devide = x / y;
+
+var final = Math.floor(devide);
+
+console.log(devide);
+
+// no:3,C
+
+var x=20;
+
+var y=3;
+
+var devide = x / y;
+
+var final = Math.floor(devide)*y;
+
+remainerIs = x-final;
+
+console.log(remainerIs);
+
+console.log(51 % 2)
+
+/*
+/ Bitwise Operators
+/ AND & &&;
+
+/ RR | ||;
+
+/ NOT~ !=;
+XOR ^;
+LEFT SHIFT <<;
+RIGHT SHIFT >>;
+
+*/
